c5e5fd8ccb2e5ec67bef47bf5dcf6b6ed69dbbee
Yann <groskiff@gmail.com>
2020-08-27 18:41:45 +0200

    nope

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
-from twitter_scraper import get_trends
+from twitter_scraper import get_trends, get_tweets
 
+for tweet in get_tweets('#france', pages=1):
+	print(tweet['text'])
 print(get_trends())
