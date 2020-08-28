60d6cd5f855725b2766977bd687bc8374d60f56b
Yann <groskiff@gmail.com>
2020-08-28 16:20:08 +0200

    top 10 hashtags

--- app.py
+++ app.py
@@ -4,15 +4,23 @@ import json
 
 bearer = os.environ['BEARER_TOKEN']
 
+tags_stats = {}
+
 r = requests.get('https://api.twitter.com/2/tweets/sample/stream', params={'tweet.fields':'entities'}, headers={'Authorization': 'Bearer ' + bearer}, stream=True)
 
 if r.encoding is None:
     r.encoding = 'utf-8'
 
 for line in r.iter_lines(decode_unicode=True):
-
     # filter out keep-alive new lines
     if line:
         parsed = json.loads(line)
         if 'data' in parsed and 'entities' in parsed['data'] and 'hashtags' in parsed['data']['entities']:
-            print(parsed['data']['entities']['hashtags'])
+            tags = parsed['data']['entities']['hashtags']
+            for tag in tags:
+                tag_tag = tag['tag']
+                if tag_tag not in tags_stats:
+                    tags_stats[tag_tag] = 1
+                else:
+                    tags_stats[tag_tag] += 1
+            print('new best: ' + str(sorted(tags_stats.items(), key=lambda item: item[1], reverse=True)[:10]))
