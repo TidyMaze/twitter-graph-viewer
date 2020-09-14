939c96fef38261463f5fee3c934ae1e71f2cc4a1
Yann <groskiff@gmail.com>
2020-09-14 19:50:00 +0200

    port

--- web.py
+++ web.py
@@ -3,6 +3,9 @@ import json
 import flask
 import networkx as nx
 from networkx.readwrite import json_graph
+import os
+
+PORT = os.environ.get("PORT")
 
 G = nx.barbell_graph(6, 3)
 # this d3 example uses the name attribute for the mouse-hover value,
@@ -24,5 +27,5 @@ def static_proxy():
     return app.send_static_file("force.html")
 
 
-print("\nGo to http://localhost:8000 to see the example\n")
-app.run(port=8000)
+print(f"\nGo to http://localhost:{PORT} to see the example\n")
+app.run(port=PORT)
